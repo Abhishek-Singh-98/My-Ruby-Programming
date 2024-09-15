020eb47b76dd32ef19caa47abc93b63568f79530
Abhishek-Singh-98 <avi04427@gmail.com>
2024-09-16 01:08:55 +0530

    substring problem solution done

--- Sub_string_dictonary.rb
+++ Sub_string_dictonary.rb
@@ -12,13 +12,21 @@ input = gets.chomp
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 def substrings(input, dictionary)
   count_hash = Hash.new
-  # input_arr = input.split.join.chars
+  stack_arr = input.downcase.chars
   #logic for key value assignment
   dictionary.each do |word|
-    puts word
-    if input.include?(word)
-      count_hash["#{word}"] ||= 0
-      count_hash["#{word}"] = input.count(word)
+    needle_arr = word.chars
+    #Needle in a Haystack modified concept
+    if (stack_arr.length >= needle_arr.length) && (stack_arr.include?(needle_arr[0]))
+      stack_arr.each_with_index do |x, i|
+        if needle_arr[0] == stack_arr[i]
+          end_count = i + needle_arr.length - 1
+          unless stack_arr[i..end_count] != needle_arr 
+            count_hash["#{word}"] ||= 0
+            count_hash["#{word}"] +=1
+          end
+        end
+      end
     end
   end
   return count_hash
